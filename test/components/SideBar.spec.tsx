b9f4097e96ce2509183cead90c475cbe00af0e24
Philipp <philipp.kessling@haw-hamburg.de>
2021-02-19 10:06:24 +0100

    make'em tests work

--- test/components/SideBar.spec.tsx
+++ test/components/SideBar.spec.tsx
@@ -1,7 +1,52 @@
+import { assert } from "chai";
 import { JSDOM } from "jsdom";
+import { h, render} from "preact";
+import { SideBar } from "../../src";
+import { ITextMenuItemOptions, TextMenuItem } from "../../src/components/panes/Text";
+import { IMenuTemplate } from "../../src/components/SideBar";
 
-const window = new JSDOM("");
+const html = '<html lang="en"><head><meta charset="UTF-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/><title>Document</title></head><body><div id="preact-root"></div></body></html>';
 
-describe("Do something", () => {
-    it("Oh, no!")
+describe("<SideBar />", () => {
+
+    let jsdom: JSDOM;
+    before(async function () {
+        jsdom = new JSDOM(html);
+        await new Promise((resolve) => {
+            global.document = jsdom.window.document;
+            jsdom.window.addEventListener("load", resolve);
+        });
+    });
+    
+    it("should instantiate with an empty items array", () => {
+        const test = <SideBar items={[]} />;
+        const root  = jsdom.window.document.getElementById("preact-root");
+        if (root !== null && jsdom.window && jsdom.window.document) {
+            // render(test, jsdom.window.document, root);
+            render(test, root);
+            assert.exists(
+                jsdom.window.document.getElementById("side-bar")
+            )
+        }
+    });
+
+    it("should accept a IMenuItem", () => {
+        const item: IMenuTemplate<string, ITextMenuItemOptions> = {
+            label: "Test",
+            type: TextMenuItem,
+            options: {
+                defaultValue: "Test"
+            }
+        };
+
+        const test = <SideBar items={[item]} />;
+        const root  = jsdom.window.document.getElementById("preact-root");
+        if (root !== null && jsdom.window && jsdom.window.document) {
+            // render(test, jsdom.window.document, root);
+            render(test, root);
+            assert.equal(
+                jsdom.window.document.getElementsByClassName("form-group-item").length, 1
+            )
+        }
+    });
 });
