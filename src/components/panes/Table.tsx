b5f2ba3036a184a0f989cdae6282074466a333a3
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-20 23:08:21 +0100

    add exports

--- src/components/panes/Table.tsx
+++ src/components/panes/Table.tsx
@@ -1,7 +1,7 @@
 import { FunctionalComponent, h, JSX } from "preact";
 import { IMenuTemplate } from "../SideBar";
 
-interface IColumnSpecification<Value, Property extends keyof Value> {
+export interface IColumnSpecification<Value, Property extends keyof Value> {
     name: string
     type: string
     editable: boolean
@@ -9,24 +9,24 @@ interface IColumnSpecification<Value, Property extends keyof Value> {
     setter?: (arg: Value[Property], property: Property, value: Value) => void
 }
 
-interface ITableOptions<Value> {
+export interface ITableOptions<Value> {
     columns: IColumnSpecification<Value, keyof Value>[];
 }
 
 // Define a few Alias function
-const TableRow: FunctionalComponent = ({ children }) => (
-    <tr>{children}</tr>
-)
+// const TableRow: FunctionalComponent = ({ children }) => (
+//     <tr>{children}</tr>
+// )
 
-const TableDataCell: FunctionalComponent = ({ children }) => (
-    <td>{children}</td>
-)
+// const TableDataCell: FunctionalComponent = ({ children }) => (
+//     <td>{children}</td>
+// )
 
 export function TableMenuItem<Value> (item: IMenuTemplate<Value[], ITableOptions<Value>>): JSX.Element {
     if (item.getter === undefined) return <div></div>
 
     return <div class="form-group-item">
-        <label>HotSpots</label>
+        <label>{item.label}</label>
         <table>
             <Header />
             <tbody>
@@ -37,17 +37,17 @@ export function TableMenuItem<Value> (item: IMenuTemplate<Value[], ITableOptions
                             item.options?.columns.map(column => {
                                 const val = value[column.property];
 
-                                // if (typeof val === "string") {
-                                //     return <td contentEditable={column.editable} onInput={(event: Event) =>{
-                                //         if (column.editable !== undefined && column.setter !== undefined) {
-                                //             column.setter(
-                                //                 (event.target as HTMLTableDataCellElement).innerText,
-                                //                 column.property,
-                                //                 value
-                                //             );
-                                //         }
-                                //     }}>{val}</td>
-                                // }
+                                if (typeof val === "string") {
+                                    return <td contentEditable={column.editable} onInput={(event: Event) =>{
+                                        if (column.editable !== undefined &&  column.editable && column.setter !== undefined) {
+                                            column.setter(
+                                                (event.target as HTMLTableDataCellElement).innerText,
+                                                column.property,
+                                                value
+                                            );
+                                        }
+                                    }}>{val}</td>
+                                }
                             })
                         }
                     </tr>             
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
+import { ButtonMenuItem } from "./components/panes/Button";
+import { ButtonGroupMenuItem, IButtonGroupOptions } from "./components/panes/ButtonGroup";
+import { CardMenuItem } from "./components/panes/Card";
+import { DisplayMenuItem } from "./components/panes/Display";
+import { DividerMenuItem } from "./components/panes/Divider";
+import { DropDownMenuItem, IDropDownMenuItemOptions } from "./components/panes/DropDown";
+import { HSliderMenuItem, IHSliderMenuItemOptions } from "./components/panes/HSlider";
+import { RichTextMenuItem } from "./components/panes/RichText";
+import { IColumnSpecification, ITableOptions, TableMenuItem } from "./components/panes/Table";
+import { TextMenuItem } from "./components/panes/Text";
+import { TextAreaMenuItem } from "./components/panes/TextArea";
 import { SideBar } from "./components/SideBar";
 
 export {
-    SideBar
+    SideBar,
+    ButtonMenuItem,
+    ButtonGroupMenuItem,
+    IButtonGroupOptions,
+    CardMenuItem,
+    DropDownMenuItem,
+    IDropDownMenuItemOptions,
+    DisplayMenuItem,
+    DividerMenuItem,
+    HSliderMenuItem,
+    IHSliderMenuItemOptions,
+    TableMenuItem,
+    TextMenuItem,
+    TextAreaMenuItem,
+    RichTextMenuItem,
+    ITableOptions,
+    IColumnSpecification
 }
