3fb23bdb9664091ace03a0a2c83a0bcab72b948d
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-20 23:07:45 +0100

    add options interface

--- src/components/panes/HSlider.tsx
+++ src/components/panes/HSlider.tsx
@@ -2,11 +2,22 @@ import { h, JSX } from "preact";
 import { useRef } from "preact/hooks";
 import { IMenuItemRenderer, IMenuTemplate } from "../SideBar";
 
+export interface IHSliderMenuItemOptions{
+    min: number
+    max: number
+    formatter: (srg: string | number) => string
+}
+
 export const HSliderMenuItem: IMenuItemRenderer = (item: IMenuTemplate): JSX.Element => {
-    const pRef = useRef<HTMLParagraphElement>();
+    const pRef = useRef<HTMLParagraphElement>(null);
 
     return <div class="form-group-item slider-item">
         <label>{item.label}</label>
+        <p ref={pRef}>{ // ref={pRef}
+            ((item.getter !== undefined) ? 
+                (item.options.formatter !== undefined) ?
+               item.options.formatter(item.getter()) : item.getter() : null)
+        }</p>
         <input
             type="range"
             min={item.options?.min}
@@ -16,9 +27,9 @@ export const HSliderMenuItem: IMenuItemRenderer = (item: IMenuTemplate): JSX.Ele
             onInput={(e: Event) => {
                 const target = e.target as HTMLInputElement
                 
-                if (item.setter && target.value && item.getter) {
+                if (item.setter && target.value) {
                     item.setter(target.value);
-                    if (pRef && pRef.current && pRef !== null && pRef.current !== null) {
+                    if (pRef !== null && pRef.current !== null && item.getter) {
                         const val = Number(item.getter());
                         const string = (item.options.formatter) ? item.options.formatter(val) : val
                         pRef.current.innerHTML = string;
@@ -26,10 +37,5 @@ export const HSliderMenuItem: IMenuItemRenderer = (item: IMenuTemplate): JSX.Ele
                 }
             }}
         />
-        <p ref={pRef}>{
-            ((item.getter !== undefined) ? 
-                (item.options.formatter !== undefined) ?
-               item.options.formatter(item.getter()) : item.getter() : null)
-        }</p>
     </div>
 }
