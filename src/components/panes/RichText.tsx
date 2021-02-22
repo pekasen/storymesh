e09b3099c736f78ace51654d9ad8a71bfd309987
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-22 12:11:36 +0100

    abolish dynamic import

--- src/components/panes/RichText.tsx
+++ src/components/panes/RichText.tsx
@@ -2,38 +2,41 @@ import { h } from "preact";
 import { useEffect, useRef } from "preact/hooks";
 import Delta from "quill-delta";
 import { IMenuItemRenderer, IMenuTemplate } from "../SideBar";
+import Quill from "quill";
 
 export const RichTextMenuItem: IMenuItemRenderer = (item: IMenuTemplate<Delta, undefined>) => {
     const ref = useRef<HTMLDivElement>(undefined);
     
     useEffect(() => {
         if (ref.current !== null) {
-            (async () => {
-                import("quill").then((Quill) => {
-                    const quill = new Quill.Quill(ref.current, {
-                        modules: {
-                            toolbar: [
-                                [{
-                                    header: [1, 2, false
-                                ]}],
-                                ["bold", "link", "underline"],
-                                ["link", "code-block"]
-                            ]
-                        }
-                    });
-        
-                    if (item.getter !== undefined) {
-                        quill.setContents(item.getter());
-                    }
-        
-                    quill.on('text-change', () => {
-                        console.log(quill.getContents());
-                        if (item.setter !== undefined) {
-                            item.setter(quill.getContents())
-                        }
-                    });
-                });                
-            })();
+            const quill = new Quill(ref.current, {
+                modules: {
+                    toolbar: [
+                        [{
+                            header: [1, 2, false
+                        ]}],
+                        ["bold", "link", "underline"],
+                        ["link", "code-block"]
+                    ]
+                }
+            });
+
+            if (item.getter !== undefined) {
+                quill.setContents(item.getter());
+            }
+
+            quill.on('text-change', () => {
+                console.log(quill.getContents());
+                if (item.setter !== undefined) {
+                    item.setter(quill.getContents())
+                }
+            });
+
+            // (async () => {
+            //     import("quill").then((Quill) => {
+                    
+            //     });                
+            // })();
         }
     });
 
