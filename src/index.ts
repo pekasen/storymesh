5aa8864cc6224cd905e9afff9ce7f724c0eeb0b9
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-22 16:02:07 +0100

    reexport everything

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
-import { ButtonMenuItem } from "./components/panes/Button";
-import { ButtonGroupMenuItem, IButtonGroupOptions } from "./components/panes/ButtonGroup";
-import { CardMenuItem } from "./components/panes/Card";
-import { DisplayMenuItem } from "./components/panes/Display";
-import { DividerMenuItem } from "./components/panes/Divider";
-import { DropDownMenuItem, IDropDownMenuItemOptions } from "./components/panes/DropDown";
-import { HSliderMenuItem, IHSliderMenuItemOptions } from "./components/panes/HSlider";
-import { RichTextMenuItem } from "./components/panes/RichText";
-import { IColumnSpecification, ITableOptions, TableMenuItem } from "./components/panes/Table";
-import { TextMenuItem } from "./components/panes/Text";
-import { TextAreaMenuItem } from "./components/panes/TextArea";
+import { Button, ButtonMenuItem } from "./components/panes/Button";
+import { ButtonGroup, ButtonGroupMenuItem, IButtonGroupOptions } from "./components/panes/ButtonGroup";
+import { Card, CardMenuItem } from "./components/panes/Card";
+import { Display, DisplayMenuItem } from "./components/panes/Display";
+import { Divider, DividerMenuItem } from "./components/panes/Divider";
+import { DropDown, DropDownMenuItem, IDropDownMenuItemOptions } from "./components/panes/DropDown";
+import { HSlider, HSliderMenuItem, IHSliderMenuItemOptions } from "./components/panes/HSlider";
+import { RichText, RichTextMenuItem } from "./components/panes/RichText";
+import { IColumnSpecification, ITableOptions, Table, TableMenuItem } from "./components/panes/Table";
+import { Text, TextMenuItem } from "./components/panes/Text";
+import { TextArea, TextAreaMenuItem } from "./components/panes/TextArea";
 import { SideBar } from "./components/SideBar";
 
 export {
     SideBar,
+    Button,
     ButtonMenuItem,
     ButtonGroupMenuItem,
+    ButtonGroup,
     IButtonGroupOptions,
     CardMenuItem,
+    Card,
     DropDownMenuItem,
+    DropDown,
     IDropDownMenuItemOptions,
     DisplayMenuItem,
+    Display,
     DividerMenuItem,
+    Divider,
     HSliderMenuItem,
+    HSlider,
     IHSliderMenuItemOptions,
     TableMenuItem,
-    TextMenuItem,
-    TextAreaMenuItem,
-    RichTextMenuItem,
+    Table,
     ITableOptions,
-    IColumnSpecification
+    IColumnSpecification,
+    TextMenuItem,
+    Text,
+    TextAreaMenuItem,
+    TextArea,
+    RichTextMenuItem,
+    RichText,
 }
