5c462912182fc6b4b39288538e155b0e07ff5435
Philipp Kessling <philipp.kessling@haw-hamburg.de>
2021-02-25 21:21:10 +0100

    [add] whitespace

--- src/classes/MenuTemplate.ts
+++ src/classes/MenuTemplate.ts
@@ -1,6 +1,6 @@
 import { IMenuItemRenderer, IMenuTemplate } from "../components/SideBar";
 
-export abstract class MenuTemplate<Value = any, Options = any> implements IMenuTemplate<Value, Options>{
+export abstract class MenuTemplate<Value = any, Options = any> implements IMenuTemplate<Value, Options> {
     public abstract type: IMenuItemRenderer;
     public abstract label: string;
     public abstract options: Options;
