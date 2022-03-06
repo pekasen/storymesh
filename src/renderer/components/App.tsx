37c165e6ed840ca920b2831158ebcd460cde02a8
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-03-06 23:58:19 +0100

    [remove] babylon.js

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,47 +1,18 @@
 import { h } from "preact";
-import { reaction } from 'mobx';
 import Logger from "js-logger";
 
 import { Header } from './Header';
 import { ThemedWindowContent, Window } from "./Window";
 import { Store } from '..';
-import { useContext, useEffect, useRef, useState } from 'preact/hooks';
+import { useContext, useRef, useState } from 'preact/hooks';
 import { EditorPaneGroup } from './EditorPaneGroup';
 import { NotificationView } from './NotificationView/NotificationView';
-import * as BABYLON from "babylonjs";
-
-// TODO: remove BABYLON.js for now
-export let rootEngine: BABYLON.Engine;
 
 export const App = (): h.JSX.Element => {
     const [, setState] = useState({});
     const store = useContext(Store);
     const canvasRef = useRef(null);
     
-    useEffect(() => {
-        if (canvasRef && canvasRef.current) {
-            rootEngine = new BABYLON.Engine(canvasRef.current, true);
-            // rootEngine.
-        }
-
-        // const disposer = reaction(
-        //     () => [
-        //         // doesn't ever trigger, remove:
-        //         // store,
-        //         // TODO: defer these both reactions to the appropriate component
-        //         store.uistate.windowProperties.title,
-        //         store.userPreferences.theme],
-        //     (root) => {
-        //         Logger.info("changed@root", root);
-        //         setState({})
-        //     }
-        // )
-
-        // return () => {
-        //     disposer();
-        // }
-    });
-
     return <Window>
             <canvas width="0px" height="0px" style="width: 0px; height: 0px;" ref={canvasRef} />
             <Header
