f574cda11d419f148d1f80add9e9b5a3b37bf80e
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-03-12 20:59:04 +0100

    [conform] linting rules

--- packages/preact-mdparse/spec/Markdown.spec.ts
+++ packages/preact-mdparse/spec/Markdown.spec.ts
@@ -3,7 +3,13 @@ import { describe, it } from 'mocha'
 
 import { MDAST } from '../src/index'
 
-export function makeLineTest(tag: string, text: string) {
+/** Runs a series of automated tests for line tags
+ * 
+ * @param  {string} tag the tag to test
+ * @param  {string} text test string
+ * @returns {void}
+ */
+function makeLineTest(tag: string, text: string): void {
   const tags: Record<string, string> = {
     h1: '#',
     h2: '##',
@@ -18,7 +24,8 @@ export function makeLineTest(tag: string, text: string) {
   
   const cases: {input: string, expected: {tag: string, text: string}, description: string}[] = [] 
   // plain and pad left
-  const a = ["", " ", "  ", "   "].forEach(e => {
+  const a = ["", " ", "  ", "   "]
+  a.forEach(e => {
     cases.push({
       input: e + tags[tag] + " " + text,
       expected: {tag: tag, text: text},
@@ -26,83 +33,195 @@ export function makeLineTest(tag: string, text: string) {
     })
   })
   // pad right
-  const b = ["", " ", "         ", "             "].forEach(e => {
+  a.forEach(e => {
     cases.push({
       input: tags[tag] + " " + text + e,
       expected: {tag: tag, text: text},
       description: `should interpret ${tags[tag]} as ${tag}`
     })
   })
-  cases.forEach((_case) => {it(_case.description, () => 
+  cases.forEach((_case) => {it(`${_case.description} for input: "${_case.input}"`, () => 
     assert.deepEqual(new MDAST(_case.input).repr(), _case.expected))
   })
   
   // fail if whitespace is missing
 }
 
+// function makeInlineTest(tag: string, text: string) {
+
+// }
+
+// const large = `
+// # Ignesque Icarus frontis dubium stant arma habenti
+
+// ## Crabronum datura igitur raptaque
+
+// Lorem markdownum stimulataque cribri grata, est, nato debilis ferreus aequora me
+// quatenus ripae amplexu Perseus. Gemitum _locum_ faceret, fumis parte respondit
+// traiecit peregit, repertum. A subegit tenaci patres more faverat tectoque exiguo
+// ad ictibus, male qui oculos gesserit et. Claudit nomine, coepta. Nil nigro nec
+// ferre huic Libye, et vulnere ambit manus: viribus.
+
+// > Exsultatque placeas sedes ferrum. Ars enim Cytherea. Sed auribus minus ex
+// > licuit resupinus sculpsit tenus vel nympharum tuta saniemque agendum atque et
+// > mentis erant. Bacis hunc urbe de doloris utque.
+
+// **Cura** natus dixit saepe **ne omnem**. Puppi Eurystheus tum, ut umbra et duo
+// videtur, manu vultus animum et undas obstipuit miscuit, sintque pro. **Fuit**
+// facit virtus inquit, per est est et arcus en sonuit. Pars Romana et felix
+// paretur: est speravit exemploque omnes. Velant tua aevum at profusis per dixerat
+// lumina, Eurytus, et.
+
+// > Iunctura timor barba letataque te urguet misero legem, statione praemiaque
+// > libidine adhuc texerat exstabant infelix rediit congerie vocabat. Exire fuit
+// > hic esse ramum, pugnantem, tenet relevare salutant creati aquarum. Est tamquam
+// > viaque. Est **quo** pulverulenta erant Atlantiades petit, pellis sinus esto
+// > tyranno decorem.
+
+// ## Narrata iterum
+
+// **Nostros mallet procubuit** quoque novantur sceleratus _ipso_ pabula. Simul in
+// primi per movere litore nequeunt praecipue dextra tertius obtulit post. Ense
+// medio. Cum et, non praestem tulit annum silet, iuxta Tartara nymphae habemus
+// inpositum pericula prius. Poenas hamadryadas ipsum solet sed, labori nec aethera
+// altaria spatioso, sub excidit ferat truncoque valle!
+
+//     var ipx = hardCopyrightRate(dcim_bar_backside);
+//     web_abend_market = servlet;
+//     megapixel -= animatedHibernate;
+
+// Viri matri effuge sceleratus reddere peremptis sacra, oras aliter fuisset mira
+// _infestae_ pascua nostrum venerat mellaque conspexit ternis, lacrimas. Si viridi
+// toros petit de laeta, Amphrysos Siculaeque audit. Inmodicum timuere pendebat
+// protinus sciet derantque habent vidisse flammis coctilibus lupum, parabat
+// litora? Et nobis, effugiunt. Est avulsa parvae poscere rutilasque, se naris
+// populare!
+
+// > Est pronus Anius, Abantiades vestrae conscia _auxiliaribus_ dies prodest,
+// > palmis esse incurvae **reducunt**. Tenebant feram creator arcem. Nisi **sed
+// > concrevit**, coniuge per? Non aut seque, loci lucum viderat iano Livor femina
+// > poenas.
+
+// Adfusique _arcanaque_ tamen ripae dixit ille nepos longum frustra pallor ignes
+// quae. Neque sub re, **suis incaluere taurus** erit! Tum antra delubra torvos.
+// Sic populus et fessa.
+
+// `
+
 describe('MDAST', () => {
     describe('MD Items', () => {
-        it('should split markdown strings by "\\n\\n"', () => {
-            const markdown = `# Headline
+//         it('should split markdown strings by "\\n\\n"', () => {
+//             const markdown = `# Headline
 
-Here's text.
-And here's some more.
+// Here's text.
+// And here's some more.
 
-And here a listing:
-- a
-- b
-+ c
-- d
+// And here a listing:
+// - a
+// - b
+// + c
+// - d
 
----    
+// ---    
 
-  ### Here's another **headline**!
+//   ### Here's another **headline**!
 
-Here's some quoted stuff:
+// Here's some quoted stuff:
 
-> Blllaasdsssa
-> and antoher blockquote
+// > Blllaasdsssa
+// > and antoher blockquote
 
-And here is a code block:
+// And here is a code block:
 
-            const x = 1
-`
+//             const x = 1
+// `
 
-            const ret = new MDAST(markdown)
-            console.dir(ret.repr(), {depth: null})
-            const expected = {
-                tag: 'array',
-                text: [
-                  { tag: 'h1', text: 'Headline' },
-                  {
-                    tag: 'p',
-                    text: "Here's text. And here's some more. And here a listing:"
-                  },
-                  {
-                    tag: 'ulist',
-                    text: [
-                      { tag: 'ul', text: 'a' },
-                      { tag: 'ul', text: 'b' },
-                      { tag: 'ul', text: 'c' },
-                      { tag: 'ul', text: 'd' }
-                    ]
-                  },
-                  { tag: 'hline', text: '' },
-                  { tag: 'h3', text: "Here's another **headline**!" },
-                  { tag: 'p', text: "Here's some quoted stuff:" },
-                  { tag: 'quote', text: 'Blllaasdsssa and antoher blockquote' },
-                  { tag: 'p', text: 'And here is a code block:' },
-                  { tag: 'code', text: 'const x = 1' }
-                ]
-              }
-            assert.deepEqual(ret.repr(), expected)
+//             const ret = new MDAST(markdown)
+//             console.dir(ret.repr(), {depth: null})
+//             const expected = {
+//                 tag: 'array',
+//                 text: [
+//                   { tag: 'h1', text: 'Headline' },
+//                   {
+//                     tag: 'p',
+//                     text: "Here's text. And here's some more. And here a listing:"
+//                   },
+//                   {
+//                     tag: 'ulist',
+//                     text: [
+//                       { tag: 'ul', text: 'a' },
+//                       { tag: 'ul', text: 'b' },
+//                       { tag: 'ul', text: 'c' },
+//                       { tag: 'ul', text: 'd' }
+//                     ]
+//                   },
+//                   { tag: 'hline', text: '' },
+//                   { tag: 'h3', text: "Here's another **headline**!" },
+//                   { tag: 'p', text: "Here's some quoted stuff:" },
+//                   { tag: 'quote', text: 'Blllaasdsssa and antoher blockquote' },
+//                   { tag: 'p', text: 'And here is a code block:' },
+//                   { tag: 'code', text: 'const x = 1' }
+//                 ]
+//               }
+//             assert.deepEqual(ret.repr(), expected)
+//         })
+        it('should interpret **w+** as bold', () => {
+          const md = 'Here is some **bold text** and here some **more**.'
+          const expected = {tag: 'p', text: [
+            'Here is some ',
+            {tag: 'strong', text: 'bold text'},
+            ' and here some ',
+            {tag: 'strong', text: 'more'},
+            '.'
+          ]}
+          const repr = new MDAST(md).repr()
+          assert.deepEqual(repr, expected)
         })
-        it('should construct a nested object from a markdown string')
-        it('should interpret **\w+** as bold')
+        it('should interpret __w+__ as italics', () => {
+          const md = 'Here is some __italics text__ and here some __more__.'
+          const expected = {tag: 'p', text: [
+            'Here is some ',
+            {tag: 'em', text: 'italics text'},
+            ' and here some ',
+            {tag: 'em', text: 'more'},
+            '.'
+          ]}
+          const repr = new MDAST(md).repr()
+          assert.deepEqual(repr, expected)
+        })
+        it('should interpret handle nested inline styles', () => {
+          const md = 'Here is some **italics __text__** and some more **bold**.'
+          const expected = {tag: 'p', text: [
+            'Here is some ',
+            {tag: 'strong', text: ['italics', {tag: 'em', text: 'text'}]},
+            ' and some more ',
+            {tag: 'strong', text: 'bold'},
+            '.'
+          ]}
+          const repr = new MDAST(md).repr()
+          assert.deepEqual(repr, expected)
+        })
         it('should interpret # as h1', () => {
           const md = "# Headline"
           const repr = new MDAST(md).repr()
           const expected = {tag: 'h1', text: 'Headline'}
+
+          assert.deepEqual(repr, expected)
+        })
+        it('should handle multiline string', () => {
+          const md = `# Headline
+
+Here comes the **sun**
+
+
+Here comes the __moon__.`
+          const repr = new MDAST(md).repr()
+          console.log(repr)
+          const expected = {tag: 'array', text: [
+            {tag: 'h1', text: 'Headline'},
+            {tag: 'p', text: ['Here comes the ', {tag: 'strong', text: 'sun'}]},
+            {tag :'p', text: ['Here comes the ', {tag: 'em', text: 'moon'}, '.']}
+          ]}
 
           assert.deepEqual(repr, expected)
         })
--- packages/preact-mdparse/src/index.tsx
+++ packages/preact-mdparse/src/index.tsx
@@ -1,8 +1,7 @@
-import { Component, h } from 'preact'
+import { Component, h, ComponentChild } from 'preact'
 
 export class MDAST {
-    // @ts-ignore
-    public text: string | MDAST | MDAST[]
+    public text: string | MDAST | (string | MDAST)[]
     public tag: string | undefined
 
     private paragraphTerminator = /\n\s*\n/gm
@@ -14,65 +13,105 @@ export class MDAST {
         {tag: 'h4', regex: /^\s{0,3}#{4}\s/g, remove: true},
         {tag: 'h5', regex: /^\s{0,3}#{5}\s/g, remove: true},
         {tag: 'h6', regex: /^\s{0,3}#{6}\s/g, remove: true},
-        {tag: 'ul', regex: /^\s{0,3}[-|\*|\+]\s/g, remove: true, block: true},
+        {tag: 'ul', regex: /^\s{0,3}[-|*|+]\s/g, remove: true, block: true},
         {tag: 'ol', regex: /^\s{0,3}\d+\s/g, remove: true, block: true},
-        {tag: 'quote', regex: /^\s{0,3}\>\s/g, remove: true, block: true},
+        {tag: 'quote', regex: /^\s{0,3}>\s/g, remove: true, block: true},
         {tag: 'code', regex: /^\s{4,}/g, remove: true},
-        {tag: 'hline', regex: /^\s{0,3}[-|\*»\+|\s*]{3,}\s{0,}/g, remove: true},
-        {tag: 'p', regex: /^\w+/g, remove: false}
+        {tag: 'hline', regex: /^\s{0,3}[-|*|+]{3,}\s{0,}/g, remove: true},
+        {tag: 'p', regex: /^\s{0,3}[\w|\s]*/g, remove: false}
     ]
     private lineRules = [
-        {tag: 'bold', regex: /\*{2}.*\*{2}/},
-        {tag: 'ital', regex: /\_{2}.*\_{2}/},
+        {tag: 'strong', regex: /\*{2}\w.*?\*{2}/gm},
+        {tag: 'em', regex: /_{2}\w.*?_{2}/gm},
     ]
 
     public constructor(text: string | MDAST | (string | MDAST)[], tag?: string) {
-        if (tag === undefined) {
-            if (typeof text === 'string') {
-                if (this.paragraphTerminator.test(text)) {
-                    this.text = new MDAST(text.split(this.paragraphTerminator))
-                }
-                if (this.lineterminator.test(text)) {
-                    this.text = new MDAST(text.split(this.lineterminator))
-                }
-                else {
-                    const { _text, _tag } = this.processLineStarts(text)
-                    this.text = _text
-                    this.tag = _tag
-                }
-            } else if (Array.isArray(text)) {
-                // reduce the array and try to assemble nodes into one
-                this.text = text.reduce<MDAST[]>((agg, cur) => {
-                cur = (typeof cur === 'string') ? new MDAST(cur) : cur
-                if (agg.length > 1) {
-                    const last = agg.pop()
-                    const thing = last?.union(cur)
-                    if (thing) agg.push(...thing)
-                } else agg.push(cur)
-                return agg
-                }, [])
-                this.tag = 'array'
+        this.text = text
+        if (typeof text === 'string') {
+            if (this.paragraphTerminator.test(text)) {
+                this.text = new MDAST(text.split(this.paragraphTerminator))
+            }
+            if (this.lineterminator.test(text)) {
+                this.text = new MDAST(text.split(this.lineterminator))
+            }
+            else {
+                const { _text, _tag } = this.processLineStarts(text)
+                this.text = this.processInline(_text) || _text
+                this.tag = _tag
             }
         } else {
-            this.text = text as string
-            this.tag = tag
+            if (tag === undefined) {
+                if (Array.isArray(text)) {
+                    // reduce the array and try to assemble nodes into one
+                    this.text = text.reduce<MDAST[]>((agg, cur) => {
+                    cur = (typeof cur === 'string') ? new MDAST(cur) : cur
+                    if (agg.length > 1) {
+                        const last = agg.pop()
+                        const thing = last?.union(cur)
+                        if (thing) agg.push(...thing)
+                    } else agg.push(cur)
+                    return agg
+                    }, [])
+                    this.tag = 'array'
+                }
+            } else {
+                this.tag = tag
+            }
         }
         this.prune()
     }
 
+    private processInline(text: string): (string | MDAST)[] | undefined {
+        // let carry = text
+        const _ret = this.lineRules.reduce<(string | MDAST)[]>((agg, rule) => {       
+            const _agg: (string | MDAST)[] = []
+
+            agg.forEach((text => {
+                if (typeof text === 'string') {
+                    if (rule.regex.test(text)) {
+                        let carry = text
+                        const matches = text.match(rule.regex)
+                        if (matches !== null) {
+                            matches.forEach(match => {
+                                const [before, after] = carry.split(match)
+                                const cleaned = match.substring(2, match.length -2)
+                                _agg.push(before, new MDAST(cleaned, rule.tag))
+                                carry = after
+                            })
+                        }
+                        _agg.push(carry)
+                    } else {
+                        _agg.push(text)
+                    }
+                } else {
+                    _agg.push(text)
+                }
+            }))
+            return _agg
+        }, [text])
+
+        if (_ret.length <= 1) {
+            // _ret.push(carry)
+            return undefined
+        }
+        return _ret.filter(e => (e !== undefined))
+    }
+
     private processLineStarts(text: string): {_tag: string | undefined, _text: string} {
         return this.lineStartRules.reduce<{_tag: string | undefined, _text: string} | undefined>((agg, rule) => {
-            if (agg === undefined && text.match(rule.regex))
+            text = text.trimEnd()
+            if (agg === undefined && text.match(rule.regex)) {
+                if (rule.tag === 'p') text = text.trimStart()
                 return {
                     _text: (rule.remove) ? text.replace(rule.regex, '') : text,
                     _tag: rule.tag
-                }
+                }}
             else
                 return agg
         }, undefined) || {_text: text, _tag: undefined}
     }
 
-    public prune() {
+    public prune(): void {
         if (this.text instanceof MDAST) {
             const other = this.text
             other.prune()
@@ -84,8 +123,17 @@ export class MDAST {
     public union(other: MDAST): MDAST[] {
         if (this.tag === 'ul' && other.tag == 'ul') {
             return [new MDAST([this, other], 'ulist')]
-        } else if (this.tag === other.tag) {
-            this.text = [this.text, other.text].join(' ')
+        } else if (this.tag === 'p' && other.tag === 'p' ) {
+            if (Array.isArray(this.text)) {
+                if (Array.isArray(other.text)) this.text = [...this.text, ...other.text]
+                else this.text = [...this.text, other.text]
+            } else {
+                if (Array.isArray(other.text)) this.text = [this.text, ...other.text]
+                else this.text = [this.text, other.text]
+            }
+
+            this.text = this.text.filter(e => e !== '')
+
             return [this]
         } else if (this.tag === 'ulist' && other.tag == 'ul') {
             if (Array.isArray(this.text)) {
@@ -102,6 +150,7 @@ export class MDAST {
     }
 
     public toString(): string {
+        console.log('MarkdownAST coerced to string')
         if (Array.isArray(this.text)) return this.text.map(e => (
             (typeof e === 'string') ? e : e.toString()
         )).join(' ')
@@ -109,18 +158,18 @@ export class MDAST {
         else return this.text.toString()
     }
 
-    public repr() : any{
+    public repr() : unknown{
         return {
             tag: this.tag,
             text: (typeof this.text === 'string') ? 
                 this.text : (Array.isArray(this.text)) ? 
-                    this.text.map(e => e.repr()) : this.text.repr()
+                    this.text.map(e => (typeof e === 'string') ? e : e.repr()) : this.text.repr()
         }
     }
 
-    public map(callback: (text: string, tag?: string) => any) : any {
+    public map<T>(callback: (text: string | (string | T)[], tag?: string) => T) : T {
         if (Array.isArray(this.text)) {
-            return this.text.map(e => e.map(callback))
+            return callback(this.text.map(e => (typeof e === 'string') ? e : e.map(callback)), this.tag)
         } else if (typeof this.text === 'string') {
             return callback(this.text, this.tag)
         } else return this.text.map(callback)
@@ -132,19 +181,16 @@ export interface Mapper {
     newtag: string
 }
 
-export function translate(ast: MDAST, mapper: Mapper[]) {
-
-}
-
 export interface MarkdownProps {
     markdown: string
 }
 
 export class Markdown extends Component<MarkdownProps> {
 
-    public render({ markdown }: MarkdownProps) {
+    public render({ markdown }: MarkdownProps) : ComponentChild {
         const ast = new MDAST(markdown)
+        console.log('Markdown AST:', ast.repr())
 
-        return ast.map((text, tag) => h(tag!, {children: text}))
+        return ast.map((text, tag) => h(tag || 'p', {children: text}))
     }
 }
